f60921b296be729640b3a8a8eef5086d0f7e0e95
hanifw00 <hanifwahyudi00@gmail.com>
2026-01-19 07:03:52 +0700

    Remove API config from tracking

--- lib/config/api_config.dart
+++
@@ -1,5 +0,0 @@
-class ApiConfig {
-  static const String apiKey = '1badc50e5318cb5b88d7641bee85c892';
-  static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-}
